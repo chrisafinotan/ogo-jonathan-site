0f9ddf58653a7862c76ab7ddf03b0ca1b707288b
chrisafinotan <iafinotan@yahoo.com>
2023-02-17 17:54:06 -0500

    trying fix

--- firebase/index.js
+++ firebase/index.js
@@ -3,9 +3,13 @@ import { getStorage } from 'firebase/storage';
 import { firebaseConfig } from './config';
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
-
 if (!app) {
    throw new Error('Firebase application could not initialized!');
 }
 
-export const storage = getStorage(app);
+const storage = getStorage(app);
+if (!storage) {
+   throw new Error('Firebase storage could not initialized!');
+}
+
+export default storage;

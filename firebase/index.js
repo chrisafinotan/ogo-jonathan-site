51c30e2106a8fee1b23f073097bf2b3696250520
chrisafinotan <iafinotan@yahoo.com>
2023-02-17 17:35:00 -0500

    Correct storage instance

--- firebase/index.js
+++ firebase/index.js
@@ -1,6 +1,11 @@
-import { initializeApp } from 'firebase/app';
-import { getStorage, ref } from 'firebase/storage';
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getStorage } from 'firebase/storage';
 import { firebaseConfig } from './config';
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+
+if (!app) {
+   throw new Error('Firebase application could not initialized!');
+}
+
 export const storage = getStorage(app);
